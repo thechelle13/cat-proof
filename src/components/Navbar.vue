2a4c5d70b01770d2af8a2f26eea0ad8da7848af2
Michelle Totherow <tym4me13@gmail.com>
2024-02-17 22:44:01 -0600

    refactor to add random cat photo to home and styling to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,15 @@
 <template>
   <nav class="navbar">
-    <ul>
+    <ul class="nav-links">
       <li><router-link to="/" class="nav-link">Home</router-link></li>
       <li><router-link to="/products" class="nav-link">Products</router-link></li>
-      <li><router-link to="/products" class="nav-link">Cart</router-link></li>
+      <li><router-link to="/cart" class="nav-link">Cart</router-link></li>
       <!-- Add more links as needed -->
     </ul>
   </nav>
 </template>
 
 <script setup>
-
 // Optionally, import any other dependencies you may need
 </script>
 
@@ -20,15 +19,16 @@
   padding: 10px;
 }
 
-.navbar ul {
+.nav-links {
   list-style-type: none;
   margin: 0;
   padding: 0;
+  display: flex;
+  justify-content: space-evenly; /* Add this line */
 }
 
 .navbar li {
   display: inline;
-  margin-right: 10px;
 }
 
 .navbar li .nav-link {
