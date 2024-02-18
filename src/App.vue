8ee4a8ae9b8171361e9ccd25247c11cfd8d8f1d3
Michelle Totherow <tym4me13@gmail.com>
2024-02-17 22:22:31 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- Navbar Component -->
+    <Navbar />
+
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+
+    <!-- About Section -->
+    <section class="about">
+      <h2>About</h2>
+      <p>This app is about products that cat people can purchase products that are used for cat-proofing their home so that sharing their home with a feline can be more pleasant.</p>
+    </section>
+
+    <!-- HelloWorld Component -->
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.about {
+  margin-top: 20px;
+}
+.about h2 {
+  font-size: 1.5em;
+}
+.about p {
+  font-size: 1.2em;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li><router-link to="/" class="nav-link">Home</router-link></li>
+      <li><router-link to="/products" class="nav-link">Products</router-link></li>
+      <li><router-link to="/products" class="nav-link">Cart</router-link></li>
+      <!-- Add more links as needed -->
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+// Optionally, import any other dependencies you may need
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #333;
+  padding: 10px;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar li {
+  display: inline;
+  margin-right: 10px;
+}
+
+.navbar li .nav-link {
+  color: red;
+  text-decoration: none;
+}
+
+.navbar li .nav-link:hover {
+  color: white;
+  text-decoration: underline;
+}
+</style>
